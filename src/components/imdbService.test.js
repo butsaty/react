7e6572c5c497f32a940eacee037a4447f9b363ab
nikolai_artemev <nikolai_artemev@epam.com>
2018-12-28 12:08:28 +0100

    todo fetch mocking

---
+++ src/components/imdbService.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ImdbService from './imdbService';
+
+ it.skip('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+ });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+//TODO Implement fetch mocking
+//global.fetch = require('jest-fetch-mock');
+
 configure({ adapter: new Adapter() });
