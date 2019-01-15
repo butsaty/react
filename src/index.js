2f01c7eb99833e0c6d384697cf5f30a6d8f03464
nikolai_artemev <nikolai_artemev@epam.com>
2019-01-15 17:18:59 +0100

    dirty code N++

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/app';
-import store from './store/configureStore';
+import store from './store';
 
 render(
     <Provider store={store}>
