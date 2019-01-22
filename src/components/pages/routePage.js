7253af06302d42d6d899ffa0502472a5dc99f5a5
nikolai_artemev <nikolai_artemev@epam.com>
2019-01-22 10:23:06 +0100

    Page not found added

--- src/components/pages/routePage.js
+++ src/components/pages/routePage.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import MainPage from './mainPage';
 import MovieDetailsPage from './movieDetailsPage';
+import NotFoundPage from './notFoundPage';
 
 class RoutePage extends React.Component {
     constructor(props) {
@@ -19,6 +20,7 @@ class RoutePage extends React.Component {
                 <Route exact path="/" render={mainPage} />
                 <Route path="/movies/:id" component={MovieDetailsPage} />
                 <Route path="/movies" render={mainPage} />
+                <Route component={NotFoundPage} />
             </Switch>
         )
     }
