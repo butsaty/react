31339465040845ed5cc484dd111afde11b91842f
nikolai_artemev <nikolai_artemev@epam.com>
2019-02-07 15:27:54 +0100

    eslint added based on airbnb

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,41 +1,41 @@
 import {
-    FETCH_MOVIES_BEGIN,
-    FETCH_MOVIES_FAILURE,
-    FETCH_MOVIES_SUCCESS
-} from "./actionTypes";
+  FETCH_MOVIES_BEGIN,
+  FETCH_MOVIES_FAILURE,
+  FETCH_MOVIES_SUCCESS,
+} from './actionTypes';
 
 export function fetchMovies(searchBy, value, order) {
-    return dispatch => {
-        dispatch(fetchBegin());
-        return fetch(`http://react-cdp-api.herokuapp.com/movies?search=${value}&searchBy=${searchBy}&sortBy=${order}&sortOrder=asc`)
-            .then(handleErrors)
-            .then(res => res.json())
-            .then(json => {
-                dispatch(fetchSuccess(json));
-                return json;
-            })
-            .catch(error => dispatch(fetchFailure(error)));
-    };
+  return (dispatch) => {
+    dispatch(fetchBegin());
+    return fetch(`http://react-cdp-api.herokuapp.com/movies?search=${value}&searchBy=${searchBy}&sortBy=${order}&sortOrder=asc`)
+      .then(handleErrors)
+      .then(res => res.json())
+      .then((json) => {
+        dispatch(fetchSuccess(json));
+        return json;
+      })
+      .catch(error => dispatch(fetchFailure(error)));
+  };
 }
 
 
 function handleErrors(response) {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
 }
 
 const fetchBegin = () => ({
-    type: FETCH_MOVIES_BEGIN
+  type: FETCH_MOVIES_BEGIN,
 });
 
 const fetchSuccess = items => ({
-    type: FETCH_MOVIES_SUCCESS,
-    payload: { items }
+  type: FETCH_MOVIES_SUCCESS,
+  payload: { items },
 });
 
 const fetchFailure = error => ({
-    type: FETCH_MOVIES_FAILURE,
-    payload: { error }
+  type: FETCH_MOVIES_FAILURE,
+  payload: { error },
 });
--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,26 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
 // export default function configureStore() {
 //   return createStore(
 //     rootReducer,
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && 
+//     window.__REDUX_DEVTOOLS_EXTENSION__ &&
 //     window.__REDUX_DEVTOOLS_EXTENSION__(),
 //     applyMiddleware(thunk)
 //   );
 // }
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-///---
-//let middleware = applyMiddleware(thunk);
-//let store = middleware(createStore)(rootReducer);
+// /---
+// let middleware = applyMiddleware(thunk);
+// let store = middleware(createStore)(rootReducer);
 
-//export default store; 
-///---
+// export default store;
+// /---
 
-export const initializeSession = ( ) => ( {
-    type: "INITIALIZE_SESSION",
-} );
+export const initializeSession = () => ({
+  type: 'INITIALIZE_SESSION',
+});
 
-export default (initialState) =>{
-    return createStore(rootReducer, initialState, applyMiddleware(thunkMiddleware));
-}
+export default initialState => createStore(rootReducer, initialState, applyMiddleware(thunkMiddleware));
